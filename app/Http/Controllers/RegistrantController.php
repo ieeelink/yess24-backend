c39c8d393df3374b56dbe97c92bdf1fd4e56b9ac
NoSineB <132141781+NoSineB@users.noreply.github.com>
2024-07-17 19:18:39 +0530

    Reverting to testing

--- app/Http/Controllers/RegistrantController.php
+++ app/Http/Controllers/RegistrantController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Registrant;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Validator;
+use phpDocumentor\Reflection\Types\True_;
 
 class RegistrantController extends Controller
 {
-    //
+    public function store_ieee(Request $request): JsonResponse|array
+    {
+        $data = $request->all()["webhook"]["answers"];
+
+        return [
+            "message" => "success",
+            "data" => $data
+        ];
+    }
+
+    public function store_non_ieee(Request $request): JsonResponse|array
+    {
+        $data = $request->all()["webhook"]["answers"];
+
+        return [
+            "message" => "success",
+            "data" => $data
+        ];
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use App\Http\Controllers\RegistrantController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(RegistrantController::class)
+    ->group(function () {
+    Route::post('/neetoform/ieee', 'store_ieee');
+    Route::post('/neetoform/non_ieee', 'store_non_ieee');
+});
+
+Route::get('/bulk/early/ieee', function () {
+    return view('bulk.early.ieee');
+});
+
+Route::get('/token', function () {
+    return csrf_token();
+});
+
+
