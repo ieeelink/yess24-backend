de88803d15ae420ad120af54071387e19a42d1ff
NoSineB <132141781+NoSineB@users.noreply.github.com>
2024-07-17 11:34:34 +0530

    Registrant and Membership Id Model Added

---
+++ database/migrations/2024_07_17_030108_create_registrants_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('registrants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('gender');
+            $table->string('college_name');
+            $table->string('t_shirt_size');
+            $table->string('food_preference');
+            $table->boolean('is_ieee_member');
+            $table->string('ticket_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('registrants');
+    }
+};
