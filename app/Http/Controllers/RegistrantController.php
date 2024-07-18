2fc3e766cd9a2a499f84e9956431df03a4cd4d30
NoSineB <132141781+NoSineB@users.noreply.github.com>
2024-07-18 08:59:14 +0530

    Data Stored in Database

--- app/Http/Controllers/RegistrantController.php
+++ app/Http/Controllers/RegistrantController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Registrant;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -17,7 +18,7 @@ class RegistrantController extends Controller
         $clean_data["phone"] = $data[2]["value"];
         $clean_data["gender"] = $data[3]["value"][0];
         $clean_data["t_shirt_size"] = $data[4]["value"][0];
-        $clean_data["food_preferences"] = $data[5]["value"][0];
+        $clean_data["food_preference"] = $data[5]["value"][0];
 
         if(isset($data[7]) && $data[6]["value"][0] == "Others"){
             $clean_data["college_name"] = $data[7]["value"];
@@ -48,6 +49,8 @@ class RegistrantController extends Controller
             "data" => $clean_data
         ];
 
+        Registrant::create($clean_data);
+
         Storage::disk('local')->put('data.json', json_encode($return_data));
 
         return $return_data;
@@ -63,6 +66,8 @@ class RegistrantController extends Controller
             "message" => "success",
             "data" => $clean_data
         ];
+
+        Registrant::create($clean_data);
 
         Storage::disk('local')->put('data.json', json_encode($return_data));
 
