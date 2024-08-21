b518336c1b01ee5b7d43f98c6f4bb4730fe7d3d9
NoSineB <132141781+NoSineB@users.noreply.github.com>
2024-08-21 17:34:34 +0530

    Export Membership ID

---
+++ app/Http/Controllers/ExportMembershipCSV.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MembershipId;
+use Illuminate\Http\Request;
+
+class ExportMembershipCSV extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $filename = 'membership.csv';
+
+        $headers = [
+            'Content-Type' => 'text/csv',
+            'Content-Disposition' => "attachment; filename=\"$filename\"",
+            'Pragma' => 'no-cache',
+            'Cache-Control' => 'must-revalidate, post-check=0, pre-check=0',
+            'Expires' => '0',
+        ];
+
+        return response()->stream(function () {
+            $handle = fopen('php://output', 'w');
+
+            // Add CSV headers
+            fputcsv($handle, [
+                'index',
+                'registrant_id',
+                'membership_id'
+            ]);
+
+            // Fetch and process data in chunks
+            MembershipId::chunk(25, function ($members) use ($handle) {
+                foreach ($members as $member) {
+                    // Extract data from each employee.
+                    $data = [
+                        $member->id ?? '',
+                        $member->registrant_id ?? '',
+                        $member->membership_id ?? '',
+                    ];
+
+                    // Write data to a CSV file.
+                    fputcsv($handle, $data);
+                }
+            });
+
+            // Close CSV file handle
+            fclose($handle);
+        }, 200, $headers);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BulkController;
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\ExportMembershipCSV;
 use App\Http\Controllers\RegistrantController;
 use App\Utils\Ticket;
 use Illuminate\Support\Facades\Route;
@@ -35,6 +36,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/events/add', [EventController::class, 'add']);
     Route::get('/events/{event}', [EventController::class, 'show']);
     Route::post('/events', [EventController::class, 'store']);
+    Route::get('/download/membership-ids', ExportMembershipCSV::class);
 });
 
 
