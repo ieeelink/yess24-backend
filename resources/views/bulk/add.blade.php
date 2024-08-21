d235cd3903f18c50cc25b826b8fe3395a25739bc
NoSineB <132141781+NoSineB@users.noreply.github.com>
2024-08-21 19:18:05 +0530

    Contributor Adding values provided

--- resources/views/bulk/add.blade.php
+++ resources/views/bulk/add.blade.php
@@ -18,7 +18,8 @@
                     <div class="mt-2">
                         <select id="ticket_type" name="ticket_type" autocomplete="ticket-type" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
                             <option value="Early Bird">Early Bird</option>
-                            <option value="Normal Registration">Normal Registration</option>
+                            <option value="Normal Ticket">Normal Registration</option>
+                            <option value="Contributor Ticket">Normal Registration</option>
                         </select>
                     </div>
                 </div>
