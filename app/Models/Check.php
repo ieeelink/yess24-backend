cdc196fe6817b706202681d0f2ce5895c9627c02
NoSineB <132141781+NoSineB@users.noreply.github.com>
2024-08-23 05:32:52 +0530

    registrant relation added to check model

--- app/Models/Check.php
+++ app/Models/Check.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Check extends Model
 {
     use HasFactory;
+
+    public function registrant()
+    {
+        return $this->belongsTo(Registrant::class);
+    }
 }
