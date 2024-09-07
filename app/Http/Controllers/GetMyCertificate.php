2999e54ced39de29ed55b20506cd3c415619797b
NoSineB <132141781+NoSineB@users.noreply.github.com>
2024-09-07 16:24:52 +0530

    Get My Certificate Code Completed

--- app/Http/Controllers/GetMyCertificate.php
+++ app/Http/Controllers/GetMyCertificate.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Registrant;
 use Illuminate\Http\Request;
 
 class GetMyCertificate extends Controller
@@ -11,6 +12,28 @@ class GetMyCertificate extends Controller
      */
     public function __invoke(Request $request)
     {
+        $validated = $request->validate([
+            'email' => 'required|email|exists:registrant,email'
+        ]);
 
+        $registrant = Registrant::where('email', $validated['email'])->first();
+
+        if (! $registrant) {
+            return response()->json([
+                'message' => 'Registrant not found, check if you have entered the email correctly',
+            ], 404);
+        }
+
+        if(! $registrant->checks->isAttending)
+        {
+            return response()->json([
+                'message' => 'It says that you were not attending the event, if there is a mistake report immediately via form given below',
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Registrant found, certificate has been generated',
+//            'certificate' => $registrant->certificate(),
+        ]);
     }
 }
